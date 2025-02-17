7489f5bab01097c926f4bf7f2bb615822b30180a
GS <GS@T3INF-14>
2025-02-17 17:12:23 +0100

    Este proyecto está creado por Daniel Godoy

---
+++ src/main/java/com/mycompany/ejemplojavadaniel2025/EjemploJavaDaniel2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplojavadaniel2025;
+
+/**
+ *
+ * @author GS
+ */
+public class EjemploJavaDaniel2025 {
+
+    public static void main(String[] args) {
+        System.out.println("hola soy Daniel Godoy");
+    }
+}
